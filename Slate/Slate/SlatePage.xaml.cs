f53fcad89d4d58a1ba9318bd1e1a17e1e7e5f721
Jeyara <Jeyara@gmail.com>
2017-01-25 09:16:42 +1100

    Inital Commit

---
+++ Slate/Slate/SlatePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Slate
+{
+    public partial class SlatePage : ContentPage
+    {
+        public SlatePage()
+        {
+            InitializeComponent();
+
+            Device.OnPlatform(
+                iOS: new Thickness(0, 20, 0, 0),
+                Android: new Thickness(0, 0, 0, 0),
+                WinPhone: new Thickness(0, 0, 0, 0)
+                );
+        }
+    }
+}
